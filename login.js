6c3d247a5bfe1bfe7ea4a723004d3cc339d1915c
Ujjwal Mittal <ujjwalmittal@live.com>
2019-04-09 17:14:55 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+function validate() {
+  var uname = document.getElementById('uname').value;
+  var pwd = document.getElementById('pwd').value;
+  // alert(uname + pwd);
+  if(pwd.length<8){
+    document.getElementById('pwd').value='';
+    alert("Password too short");
+  }
+  // window.redirect("/index.html");
+  if (pwd="qwertyuiop") {
+    alert("Valid Password");
+    window.location.href = "1.html";
+  } else {
+    alert("Invalid Password");
+  }
+}
